be99e7eed44f473c58790911d7663e260122c07d
Daniel Tesfai <dannyhd88@gmail.com>
2026-02-13 17:36:43 +0100

    Remove outdated archive file and clean up exam preparation notebook
    - Deleted the archive_filelist.csv as it is no longer needed.
    - Updated exam_prep.ipynb to remove output cells and set execution counts to null for a cleaner presentation.
    - Removed play.py as it is redundant with the new play.ipynb.
    - Added play.ipynb with improved code examples and outputs for better clarity and functionality.

--- Algorithmic-Methods/play.py
+++
@@ -1,6 +0,0 @@
-tuple = (1, 2, 3, 4, 5)
-print(tuple[0]) # Accessing the first element of the tuple
-#tuple[1] = 10 # This will raise an error because tuples are immutable
-tuple_1 = (1, 2, [1,2,3], 4, 5)
-tuple_1[2][0] = 10 # This will work because the list inside the tuple is mutable
-print(tuple_1) # Output: (1, 2, [10, 2, 3], 4, 5)
